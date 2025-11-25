ebcec9fa059d261e8995713632a69959c694aac3
willianbatist <willian.alves.b15@gmail.com>
2025-11-25 16:13:30 -0300

    feat: exerc 03, req 01

--- csharp-003-exercicio-paint-shop/src/PaintShop/Wall.cs
+++ csharp-003-exercicio-paint-shop/src/PaintShop/Wall.cs
@@ -3,11 +3,57 @@
 // 1 - Crie uma classe Wall
 public class Wall
 {
-    public double Width;
-    public double Height;
-    public double ExcludedArea;
-    public double PaintableArea;
+    private double _width;
+    public double Width
+    {
+        get { return _width; }
+        set
+        {
+            if (value <= 0)
+            {
+                _width = 1;
+            }
+            else
+            {
+                _width = value;
+            }
+        }
+    }
+
+    public double _height;
+    public double Height
+    {
+        get { return _height; }
+        set
+        {
+            if (value <= 0)
+            {
+                _height = 1;
+            }
+            else
+            {
+                _height = value;
+            }
+        }
+    }
+    private double _excludedArea;
+    public double ExcludedArea
+    {
+        get { return _excludedArea; }
+        set { _excludedArea = value; }
+    }
+    public double _paintableArea;
+    public double PaintableArea
+    {
+        get
+        {
+            double totalArea = _width * _height;
+            return totalArea - ExcludedArea;
+        }
+    }
     public Wall(double width, double height)
     {
+        this.Width = width;
+        this.Height = height;
     }
 }
