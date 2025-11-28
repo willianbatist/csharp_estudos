798df46bd30c60870ecfcd0b3d003faccec1d3f9
willianbatist <willian.alves.b15@gmail.com>
2025-11-28 16:50:31 -0300

    feat: req 02

--- csharp-003-exercicio-paint-shop/src/PaintShop/Room.cs
+++ csharp-003-exercicio-paint-shop/src/PaintShop/Room.cs
@@ -3,7 +3,27 @@ namespace PaintShop;
 // 2 - Crie uma classe Room
 public class Room
 {
-    public double TotalPaintableArea;
-    public Room(Wall[] walls)
-    {}
+    private double _totalPaintableArea;
+    public double TotalPaintableArea
+    {
+        get
+        {
+            for (int i = 0; i < walls.Length; i++)
+            {
+                _totalPaintableArea += walls[i].PaintableArea;
+            }
+
+            return _totalPaintableArea;
+        }
+    }
+    private Wall[] _walls;
+    public Wall[] walls
+    {
+        get { return _walls; }
+    }
+
+    public Room(params Wall[] walls)
+    {
+        _walls = walls;
+    }
 }
